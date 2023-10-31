b7f2dde45599717e2a1c9848a9a0b71e0200eaa1
Hentioe <meow.i5.br@gmail.com>
2023-10-31 21:02:04 +0800

    Improve build script

--- build.exs
+++ build.exs
@@ -1,24 +1,52 @@
 defmodule Build do
-  def run([full_version, base]) do
-    {major, minor, _patch} = parse_version(full_version)
+  @moduledoc false
 
-    version = "#{major}.#{minor}"
+  defmodule Version do
+    @moduledoc false
 
-    build_dockerfile(version, base)
+    defstruct [:major, :minor, :patch]
 
-    {:ok, full_version_re} = Regex.compile("Elixir\s#{full_version}")
+    @enforce_keys [:major, :minor, :patch]
+    @type t :: %__MODULE__{
+            major: non_neg_integer(),
+            minor: non_neg_integer(),
+            patch: String.t()
+          }
 
-    if get_version_output(version, base) =~ full_version_re do
+    def from(text) do
+      case String.split(text, ".") do
+        [major, minor | patch] ->
+          patch = Enum.join(patch, ".")
+
+          %__MODULE__{
+            major: String.to_integer(major),
+            minor: String.to_integer(minor),
+            patch: patch
+          }
+
+        _ ->
+          raise "Invalid version: #{text}"
+      end
+    end
+
+    def format_major_minor(version) do
+      "#{version.major}.#{version.minor}"
+    end
+  end
+
+  def run([version_text, base]) do
+    version = Version.from(version_text)
+    major_dot_minor = Version.format_major_minor(version)
+
+    build_dockerfile(major_dot_minor, base)
+
+    {:ok, full_version_re} = Regex.compile("Elixir\s#{version_text}")
+
+    if get_version_output(major_dot_minor, base) =~ full_version_re do
       IO.puts("#{IO.ANSI.green()}ok#{IO.ANSI.reset()}")
     else
       IO.puts("test #{IO.ANSI.red()}failed#{IO.ANSI.reset()}")
     end
-  end
-
-  defp parse_version(full_version) do
-    [major, minor, patch] = String.split(full_version, ".")
-
-    {major, minor, patch}
   end
 
   defp build_dockerfile(version, base) do
